ef8c1781b7601889751d6a7ca6c3112379d72969
MU-munn <rpgmukai333@gmail.com>
2022-06-28 15:11:26 +0900

    update

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -7,6 +7,9 @@ import {
   HStack,
   Link,
   Image,
+  InputGroup,
+  Input,
+  InputRightElement,
 } from "@chakra-ui/react";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
@@ -30,62 +33,107 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Box bgColor={"aliceblue"}>
-        <Box h={"100vh"}>
-          <NavbarWithAvator></NavbarWithAvator>
-          <Center h={"100px"}>
-            <Avatar
-              boxSize="10"
-              name="Christoph Winston"
-              src="https://tinyurl.com/yhkm2ek8"
-              as="a"
-              href="/VtuberCommunityPage"
-            ></Avatar>
-          </Center>
-          <Box h="320px" p={2} w="80%" m={"0 auto"}>
-            <Heading mb="5">話題のVtuber!!</Heading>
-            <HStack justifyContent={"space-between"}>
-              <Image
-                borderRadius={"50px"}
-                h={"300px"}
-                w="300px"
-                bgSize="inherit"
-                src={
-                  "https://yt3.ggpht.com/ytc/AKedOLQZ7kGnEH7CsX4Eoe79iPVh6Vp6oa2iP3_kInXO=s800-c-k-c0x00ffffff-no-rj"
-                }
-                alt="a"
-              ></Image>
-              <Image
-                borderRadius={"50px"}
-                h={"300px"}
-                w="300px"
-                bgSize="inherit"
-                src={
-                  "https://yt3.ggpht.com/BcgqkZ7KsqPj6dIT0NIHWqOLt8XcogzwdI5miNwr412ocSwGP057XIy8-ji9kk_hp6MuKPpW4w=s800-c-k-c0x00ffffff-no-rj"
-                }
-                alt="a"
-              ></Image>
-              <Image
-                borderRadius={"50px"}
-                h={"300px"}
-                w="300px"
-                bgSize="inherit"
-                src={
-                  "https://yt3.ggpht.com/BcgqkZ7KsqPj6dIT0NIHWqOLt8XcogzwdI5miNwr412ocSwGP057XIy8-ji9kk_hp6MuKPpW4w=s800-c-k-c0x00ffffff-no-rj"
-                }
-                alt="a"
-              ></Image>
-              <Image
-                borderRadius={"50px"}
-                h={"300px"}
-                w="300px"
-                bgSize="inherit"
-                src={
-                  "https://yt3.ggpht.com/BcgqkZ7KsqPj6dIT0NIHWqOLt8XcogzwdI5miNwr412ocSwGP057XIy8-ji9kk_hp6MuKPpW4w=s800-c-k-c0x00ffffff-no-rj"
-                }
-                alt="a"
-              ></Image>
-            </HStack>
-          </Box>
+        <NavbarWithAvator></NavbarWithAvator>
+        <Center h={"100px"} w="70%" m={"0 auto"}>
+          <InputGroup size="md">
+            <Input pr="4.5rem" placeholder="Enter password" />
+            <InputRightElement width="4.5rem">
+              <Button h="1.75rem" size="sm">
+                search
+              </Button>
+            </InputRightElement>
+          </InputGroup>
+        </Center>
+        <Box h="" p={2} w="80%" m={"0 auto"}>
+          <Heading mb="5">話題のVtuber!!</Heading>
+          <HStack justifyContent={"space-between"}>
+            <Image
+              borderRadius={"50px"}
+              h={"300px"}
+              w="300px"
+              bgSize="inherit"
+              src={
+                "https://yt3.ggpht.com/ytc/AKedOLQZ7kGnEH7CsX4Eoe79iPVh6Vp6oa2iP3_kInXO=s800-c-k-c0x00ffffff-no-rj"
+              }
+              alt="a"
+            ></Image>
+            <Image
+              borderRadius={"50px"}
+              h={"300px"}
+              w="300px"
+              bgSize="inherit"
+              src={
+                "https://yt3.ggpht.com/BcgqkZ7KsqPj6dIT0NIHWqOLt8XcogzwdI5miNwr412ocSwGP057XIy8-ji9kk_hp6MuKPpW4w=s800-c-k-c0x00ffffff-no-rj"
+              }
+              alt="a"
+            ></Image>
+            <Image
+              borderRadius={"50px"}
+              h={"300px"}
+              w="300px"
+              bgSize="inherit"
+              src={
+                "https://yt3.ggpht.com/BcgqkZ7KsqPj6dIT0NIHWqOLt8XcogzwdI5miNwr412ocSwGP057XIy8-ji9kk_hp6MuKPpW4w=s800-c-k-c0x00ffffff-no-rj"
+              }
+              alt="a"
+            ></Image>
+            <Image
+              borderRadius={"50px"}
+              h={"300px"}
+              w="300px"
+              bgSize="inherit"
+              src={
+                "https://yt3.ggpht.com/BcgqkZ7KsqPj6dIT0NIHWqOLt8XcogzwdI5miNwr412ocSwGP057XIy8-ji9kk_hp6MuKPpW4w=s800-c-k-c0x00ffffff-no-rj"
+              }
+              alt="a"
+            ></Image>
+          </HStack>
+        </Box>
+        <Box h={"100px"}></Box>
+        <Box h="" p={2} w="80%" m={"0 auto"}>
+          <Heading mb="5">話題のVtuber!!</Heading>
+          <HStack justifyContent={"space-between"}>
+            <Image
+              borderRadius={"50px"}
+              h={"300px"}
+              w="300px"
+              bgSize="inherit"
+              src={
+                "https://yt3.ggpht.com/ytc/AKedOLQZ7kGnEH7CsX4Eoe79iPVh6Vp6oa2iP3_kInXO=s800-c-k-c0x00ffffff-no-rj"
+              }
+              alt="a"
+            ></Image>
+            <Image
+              borderRadius={"50px"}
+              h={"300px"}
+              w="300px"
+              bgSize="inherit"
+              src={
+                "https://yt3.ggpht.com/BcgqkZ7KsqPj6dIT0NIHWqOLt8XcogzwdI5miNwr412ocSwGP057XIy8-ji9kk_hp6MuKPpW4w=s800-c-k-c0x00ffffff-no-rj"
+              }
+              alt="a"
+            ></Image>
+            <Image
+              borderRadius={"50px"}
+              h={"300px"}
+              w="300px"
+              bgSize="inherit"
+              src={
+                "https://yt3.ggpht.com/BcgqkZ7KsqPj6dIT0NIHWqOLt8XcogzwdI5miNwr412ocSwGP057XIy8-ji9kk_hp6MuKPpW4w=s800-c-k-c0x00ffffff-no-rj"
+              }
+              alt="a"
+            ></Image>
+            <Image
+              borderRadius={"50px"}
+              h={"300px"}
+              w="300px"
+              bgSize="inherit"
+              src={
+                "https://yt3.ggpht.com/BcgqkZ7KsqPj6dIT0NIHWqOLt8XcogzwdI5miNwr412ocSwGP057XIy8-ji9kk_hp6MuKPpW4w=s800-c-k-c0x00ffffff-no-rj"
+              }
+              alt="a"
+            ></Image>
+          </HStack>
         </Box>
       </Box>
     </>
