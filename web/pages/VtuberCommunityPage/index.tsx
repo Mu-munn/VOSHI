64eab4c78ccb20399de5f7e544d018a5d392ca13
tama10171 <tama10171nnm@gmail.com>
2022-06-30 13:49:41 +0900

    test 20220630

--- web/pages/VtuberCommunityPage/index.tsx
+++ web/pages/VtuberCommunityPage/index.tsx
@@ -1,28 +1,63 @@
-import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
+import {
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+  Link,
+  Image,
+  Text,
+  Heading,
+  Flex,
+  Box,
+  Stack,
+  Divider,
+} from "@chakra-ui/react";
 import { NavbarWithAvator } from "../../components/chakra/NavbarWithAvatar/App";
 export default function VtuberCommunityPage() {
   return (
     <>
-    <NavbarWithAvator></NavbarWithAvator>
-      <Tabs ml={"50px"} mr={"50px"}>
-        <TabList>
-          <Tab>Oned</Tab>
-          <Tab>Two</Tab>
-          <Tab>Three</Tab>
-        </TabList>
-
-        <TabPanels>
-          <TabPanel>
-            <p>one!</p>
-          </TabPanel>
-          <TabPanel>
-            <p>two!</p>
-          </TabPanel>
-          <TabPanel>
-            <p>three!</p>
-          </TabPanel>
-        </TabPanels>
-      </Tabs>
+      <Box backgroundColor={"aliceblue"}>
+        <NavbarWithAvator></NavbarWithAvator>
+        <Stack
+          backgroundColor={"skyblue"}
+          alignItems="center"
+          gap="2"
+          direction={["column", "row"]}
+        >
+          <Image
+            marginLeft={"30px"}
+            borderRadius={"50px"}
+            h={"300px"}
+            w="300px"
+            bgSize="inherit"
+            src={
+              "https://yt3.ggpht.com/ytc/AKedOLQZ7kGnEH7CsX4Eoe79iPVh6Vp6oa2iP3_kInXO=s800-c-k-c0x00ffffff-no-rj"
+            }
+            alt="a"
+          ></Image>
+          <Heading>
+            <div className="VtuberCommunityPageText" id="WhiteText">
+              一ノ瀬　うるは
+            </div>
+          </Heading>
+          <Link
+            href="https://www.youtube.com/channel/UC5LyYg6cCA4yHEYvtUsir3g"
+            isExternal
+          >
+            <div className="VtuberCommunityPageText" id="WhiteText">
+            Youtube
+            </div>
+          </Link>
+          <Link href="https://twitter.com/uruha_ichinose" isExternal>
+            <div className="VtuberCommunityPageText" id="WhiteText">
+            Twitter
+            </div>
+          </Link>
+        </Stack>
+        <Divider padding={"10px"} borderColor={"blue"} />
+        <Box></Box>
+      </Box>
     </>
   );
 }
