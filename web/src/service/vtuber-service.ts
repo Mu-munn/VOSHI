db244a7f818ec1a63ff96355ad3649371a36fb5b
miyadai1029 <miyadai5168@gmail.com>
2022-07-01 11:05:44 +0900

    vtuberとofficeを定義、vtuber-serviceを作成

---
+++ web/src/service/vtuber-service.ts
@@ -0,0 +1,9 @@
+import { Vtuber } from "@/project-types/vtuber"
+import { async } from "@firebase/util"
+export class VtuberService {
+    static createOrUpdataVtuber = async ({
+        vtuber
+    }:{
+        vtuber: Vtuber
+    })
+}
