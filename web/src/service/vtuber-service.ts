dc69856e5be33fdc0e741c6529a86d62a448f50b
miyadai1029 <miyadai5168@gmail.com>
2022-07-05 09:49:25 +0900

    ok

--- web/src/service/vtuber-service.ts
+++ web/src/service/vtuber-service.ts
@@ -1,9 +1,20 @@
 import { Vtuber } from "@/project-types/vtuber"
 import { async } from "@firebase/util"
+import { app } from "../utils/firebase/init"
+
+
 export class VtuberService {
     static createOrUpdataVtuber = async ({
         vtuber
+        
     }:{
-        vtuber: Vtuber
-    })
+        vtuber : Vtuber
+
+    }):Promise<void>=>{
+        //const db getFirestore(app)
+        if(vtuber)
+        {
+            
+        }
+    }
 }
