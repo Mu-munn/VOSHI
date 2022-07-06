c494253e50c4a1b72143db42c61bd6ddc300fb54
MU-munn <rpgmukai333@gmail.com>
2022-07-06 10:28:00 +0900

    create communitypage

--- web/pages/Community/index.tsx
+++ web/pages/Community/index.tsx
@@ -1,10 +1,32 @@
-import { Box } from "@chakra-ui/react"
-import { NavbarWithAvator } from "../../components/chakra/NavbarWithAvatar/App"
+import { Box, Heading, HStack } from "@chakra-ui/react";
+import { NavbarWithAvator } from "../../components/chakra/NavbarWithAvatar/App";
 
 export default function CommunityPage() {
   return (
-    <Box>
+    <Box bgColor={"aliceblue"}>
       <NavbarWithAvator></NavbarWithAvator>
+      <Box h={"60px"}> </Box>
+      <Heading
+        color={"pink"}
+        fontSize="100px"
+        left={"80px"}
+        position="absolute"
+        // textShadow={"lg"}
+        textDecorationLine="blink"
+      >
+        COMMUNITY
+      </Heading>
+      <HStack>
+        <Box w={"20%"}></Box>
+        <Box
+          w="100%"
+          bgColor={"#fa709a"}
+          py="6"
+          borderTopLeftRadius={"200px"}
+          // boxShadow="dark-lg"
+          h={"3000px"}
+        ></Box>
+      </HStack>
     </Box>
-  )
+  );
 }
