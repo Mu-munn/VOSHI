0cc1622698c7f96e90aac2eca7658f9ce6e3e424
Mu-munn <rpgmukai333@gmail.com>
2022-07-07 14:57:18 +0900

    仮完成

--- web/pages/Community/index.tsx
+++ web/pages/Community/index.tsx
@@ -1,32 +1,167 @@
-import { Box, Heading, HStack } from "@chakra-ui/react";
-import { NavbarWithAvator } from "../../components/chakra/NavbarWithAvatar/App";
+import { PhoneIcon } from "@chakra-ui/icons"
+import {
+  Box,
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  Flex,
+  Grid,
+  Heading,
+  HStack,
+  IconButton,
+  Input,
+  SimpleGrid,
+  Stack,
+  Tag,
+  TagCloseButton,
+  TagLabel,
+  useDisclosure,
+  VStack,
+} from "@chakra-ui/react"
+import React, { useState } from "react"
+import ArticleCard from "../../components/Cards/idea-card/ArticleCard/app"
+import OfficeCardTag from "../../components/Cards/idea-card/CardTag"
+import { NavbarWithAvator } from "../../components/chakra/NavbarWithAvatar/App"
 
 export default function CommunityPage() {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const btnRef = React.useRef()
+  const [test, SetTest] = useState(["aa"])
+  const SelectTag = () => {
+    test.push("aa")
+    test.push("aa")
+    test.push("aa")
+  }
+  const reptiles = ["alligator", "snake", "lizard"]
   return (
-    <Box bgColor={"aliceblue"}>
+    <Box bgColor={"aliceblue"} h='2000px'>
+      <Drawer isOpen={isOpen} placement='right' onClose={onClose} finalFocusRef={btnRef}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader>Create your account</DrawerHeader>
+
+          <DrawerBody>
+            <Input placeholder='Type here...' />
+          </DrawerBody>
+
+          <DrawerFooter>
+            <Button variant='outline' mr={3} onClick={onClose}>
+              Cancel
+            </Button>
+            <Button colorScheme='blue' onClick={SelectTag}>
+              Save
+            </Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
       <NavbarWithAvator></NavbarWithAvator>
-      <Box h={"60px"}> </Box>
-      <Heading
-        color={"pink"}
-        fontSize="100px"
-        left={"80px"}
-        position="absolute"
-        // textShadow={"lg"}
-        textDecorationLine="blink"
-      >
-        COMMUNITY
-      </Heading>
-      <HStack>
-        <Box w={"20%"}></Box>
-        <Box
-          w="100%"
-          bgColor={"#fa709a"}
-          py="6"
-          borderTopLeftRadius={"200px"}
-          // boxShadow="dark-lg"
-          h={"3000px"}
-        ></Box>
-      </HStack>
+      <Stack justify={"space-between"} w='95%' direction={"row"} m={"0 auto"} alignItems={"end"}>
+        <Heading
+       m={'0 auto'}
+          alignItems={"end"}
+          fontSize={"100px"}
+          // bgGradient='linear(to-l, #7928CA, #FF0080)'
+bgColor='#FF0080'
+          bgClip='text'
+          fontWeight='extrabold'
+        >
+          COMMUNITY
+        </Heading>
+        {/* <HStack>
+          <IconButton
+            bgColor={"#FF0080"}
+            aria-label='Call Segun'
+            size='lg'
+            icon={<PhoneIcon />}
+            borderRadius='90%'
+            color={"white"}
+          />
+          <Box w={"20px"}></Box>
+          <Button
+            bgColor='#FF0080'
+            color={"white"}
+            fontWeight={"light"}
+            onClick={onOpen}
+            ref={btnRef}
+          >
+            ADD NEW
+          </Button>
+          <Box w={"30px"}></Box>
+        </HStack> */}
+      </Stack>
+
+      <Box h={"28px"}></Box>
+
+      <Box className='content' w={"95%"} m='0 auto'>
+        <Box pos={'sticky'} top='50px' zIndex={1} bgColor='white' borderRadius={'2xl'} p={2} boxShadow='md' w='86%' m={'0 auto'}>
+        <HStack h={"70px"} bgColor=''  justifyContent={'space-between'} >
+          {/* <SimpleGrid>{test.length && <OfficeCardTag></OfficeCardTag>}</SimpleGrid> */}
+          <Flex wrap={"wrap"} w="800px" gap={1}>
+            {["green","red"].map((m) => (
+            <Tag
+            key={m}
+            borderRadius='full'
+            variant='solid'
+            colorScheme={m}
+          >
+            <TagLabel>{m}</TagLabel>
+            <TagCloseButton />
+          </Tag>
+            ))}
+          </Flex>
+          <HStack alignItems={'flex-end'}>
+            <IconButton
+             colorScheme={'blue'}
+             variant='outline'
+              aria-label='Call Segun'
+              icon={<PhoneIcon />}
+              size='md'
+              borderRadius={'full'}
+              ref={btnRef}
+              onClick={onOpen}
+            />
+            <Box w={"10px"}></Box>
+            <Button
+              bgColor='#FF0080'
+              color={"white"}
+              fontWeight={"light"}
+          
+      
+              borderRadius='full'
+            >
+              ADD NEW
+            </Button>
+            <Box w={"30px"}></Box>
+          </HStack>
+        </HStack>
+        </Box>
+       
+
+        <Box h={"28px"}></Box>
+
+        <Flex bgColor='' m={"0 auto"} wrap='wrap' columns={3} gap='2'>
+          <ArticleCard />
+          <ArticleCard />
+          <ArticleCard />
+          <ArticleCard />
+          <ArticleCard />
+          <ArticleCard />
+          <ArticleCard />
+          <ArticleCard />
+          <ArticleCard />
+          <ArticleCard />
+          <ArticleCard />
+          <ArticleCard />
+          <ArticleCard />
+          <ArticleCard />
+        </Flex>
+      </Box>
     </Box>
-  );
+  )
 }
