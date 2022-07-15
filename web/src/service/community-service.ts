dcd5492adcf07d21dd93bb81768f26382552ecd6
miyadai1029 <miyadai5168@gmail.com>
2022-07-15 10:35:20 +0900

    community登録処理完成

--- web/src/service/community-service.ts
+++ web/src/service/community-service.ts
@@ -7,13 +7,11 @@ import { v4 as uuidv4 } from 'uuid';
 export class CommunityService {
     static createCommunity = async ({
         community
-
     }: {
         community: Community
-
     }): Promise<void> => {
         const db = getFirestore(app)
-        
+
         const uuid = uuidv4()
 
         const newcommunityRef = doc(db, 'community', uuid)
@@ -22,12 +20,8 @@ export class CommunityService {
                 ...community,
                 id: uuid
             })
-
         } catch (e) {
             throw new Error('communityの更新に失敗しました')
         }
-
-
-
     }
 }
